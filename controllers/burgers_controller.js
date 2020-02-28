57c3e0f6e9c7b2c22c947db60c88aeceafbd3ebf
Brian Prickril <bprickril22@gmail.com>
2020-02-28 11:59:49 -0500

    created post route

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,15 +5,22 @@ var router = express.Router();
 // Import the model (cat.js) to use its database functions.
 var burger = require("../models/burger.js");
 
-router.get("/", function(req, res) {
-    burger.all(function(data) {
-      var hbsObject = {
-        burgers: data
-      };
-      console.log(hbsObject);
-      res.render("index", hbsObject);
+router.get("/", function (req, res) {
+    burger.all(function (data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
     });
-  });
+});
+
+router.post("/api/burgers", function (req, res) {
+    burger.create(["burger_name"], [req.body.burger_name], function (result) {
+        // Send back the ID of the new burger
+        res.json({ id: result.insertId });
+    });
+});
 
 
 
