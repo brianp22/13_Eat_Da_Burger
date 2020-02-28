bb2321ff4840a76fc08ba53231be0623801d106f
Brian Prickril <bprickril22@gmail.com>
2020-02-28 12:01:40 -0500

    fixed bugs

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,7 +6,7 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 router.get("/", function (req, res) {
-    burger.all(function (data) {
+    burger.selectAll(function (data) {
         var hbsObject = {
             burgers: data
         };
@@ -16,7 +16,7 @@ router.get("/", function (req, res) {
 });
 
 router.post("/api/burgers", function (req, res) {
-    burger.create(["burger_name"], [req.body.burger_name], function (result) {
+    burger.insertOne(["burger_name"], [req.body.burger_name], function (result) {
         // Send back the ID of the new burger
         res.json({ id: result.insertId });
     });
@@ -27,7 +27,7 @@ router.put("/api/burgers/:id", function(req, res) {
   
     console.log("condition", condition);
   
-    burger.update({
+    burger.updateOne({
       devoured: req.body.devoured
     }, condition, function(result) {
       if (result.changedRows == 0) {
