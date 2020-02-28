8fd5adefdb7b51acee42dabfce201bc71e925da3
Brian Prickril <bprickril22@gmail.com>
2020-02-28 11:57:59 -0500

    created get route

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,6 +5,16 @@ var router = express.Router();
 // Import the model (cat.js) to use its database functions.
 var burger = require("../models/burger.js");
 
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+      var hbsObject = {
+        burgers: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
+  });
+
 
 
 
